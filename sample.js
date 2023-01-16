dbbd0378cdc7ae6e5d13655e9b729b519cb078de
AntonioJesusRG <antoniogar129@gmail.com>
2023-01-16 13:45:21 +0100

    Add sample.js file

---
+++ sample.js
@@ -0,0 +1,15 @@
+x = 23;
+z = 89;
+if (true) {
+  x = 10;
+}
+
+// GIT es un gesto de versiones y se basa en la gestión de repositorios.
+// Al escribir $ git init la carpeta pasa a ser un repositorio. Lo importante es la U que aparece en la derecha del nombre de la carpeta. La U verde es Untracked, archivo nuevo que no se ha trackeado aún
+
+//Acceder a subcarpeta: cd <nombre de la carpeta>
+
+//En la carpeta git se guardan commits del código, que son "fotografias" de un estado concreto, como un historial de cambios
+//main es la rama en la que estamos
+const x = 22;
+console.log(x);
