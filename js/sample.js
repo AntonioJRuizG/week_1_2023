f6db64e95a14119a23a5f22de401900e20a9d2ff
AntonioJesusRG <antoniogar129@gmail.com>
2023-01-16 16:03:59 +0100

    Change folder

---
+++ js/sample.js
@@ -0,0 +1,23 @@
+x = 23;
+z = 89;
+if (true) {
+  x = 10;
+}
+
+// GIT es un gesto de versiones y se basa en la gestión de repositorios.
+// Al escribir $ git init la carpeta pasa a ser un repositorio. Lo importante es la U que aparece en la derecha del nombre de la carpeta. La U verde es Untracked, archivo nuevo que no se ha trackeado aún
+
+//Acceder a subcarpeta: cd <nombre de la carpeta>
+
+//En la carpeta git se guardan commits del código, que son "fotografias" de un estado concreto, como un historial de cambios
+//main es la rama en la que estamos
+const x = 22;
+console.log(x);
+// git commit -m'descripcion de lo que se commitea'. La primera vez que se hace un commit se entiende que son ficheros de configuracion y se llama 'Initial commit'
+
+//Github es un hosting de repositorios de git
+
+const sample = () => {
+  const c = 22;
+  console.log(c);
+};
